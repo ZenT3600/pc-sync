aadca318cf760058f0525d37126eb899d4a1d8ad
ZenT3600 <matteo.leggio@tiscali.it>
2023-03-24 15:18:39 +0100

    Added logging to Registry class

--- src/iis/tommasosalvini/pcrestorer/registry/Registry.java
+++ src/iis/tommasosalvini/pcrestorer/registry/Registry.java
@@ -1,5 +1,6 @@
 package iis.tommasosalvini.pcrestorer.registry;
 
+import iis.tommasosalvini.pcrestorer.logging.Log;
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidParameterException;
@@ -14,12 +15,16 @@ public class Registry {
 	    Scanner sErr = new java.util.Scanner(p.getErrorStream()).useDelimiter("\\A");
 	    if (!sErr.hasNext()) return sOut.hasNext() ? sOut.next() : "";
 	    else {
+			Log.error("Invalid permission to modify registry");
 	    	throw new NoPermissionException("Invalid permission to modify registry");
 	    }
 	}
 	
 	public static void importRegistryDump(File dump) throws InvalidParameterException, NoPermissionException, IOException {
-		if (!dump.exists()) throw new InvalidParameterException("The registry dump provided does not exist");
+		if (!dump.exists()) {
+			Log.error("The registry dump " + dump.getName() + " provided does not exist");
+			throw new InvalidParameterException("The registry dump provided does not exist");
+		}
 		System.out.println(execCmd("reg import " + dump.getAbsolutePath()));
 	}
 	
