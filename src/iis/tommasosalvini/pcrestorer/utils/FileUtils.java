ddddbf32795b1f14e58c2d4b40575b2724292de9
ZenT3600 <matteo.leggio@tiscali.it>
2023-03-24 15:17:49 +0100

    Moved utils to appropriate package

--- src/iis/tommasosalvini/pcrestorer/utils/FileUtils.java
+++ src/iis/tommasosalvini/pcrestorer/utils/FileUtils.java
@@ -1,18 +1,27 @@
-package iis.tommasosalvini.pcrestorer;
+package iis.tommasosalvini.pcrestorer.utils;
 
+import iis.tommasosalvini.pcrestorer.logging.Log;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.nio.file.AccessDeniedException;
 
 public class FileUtils {
 	
 	public static boolean compare(File f1, File f2) throws NoSuchAlgorithmException, IOException {
-		byte[] f1bytes = Files.readAllBytes(f1.toPath());
-		byte[] f2bytes = Files.readAllBytes(f2.toPath());
-		
+		byte[] f1bytes;
+		byte[] f2bytes;
+		try {
+			f1bytes = Files.readAllBytes(f1.toPath());
+			f2bytes = Files.readAllBytes(f2.toPath());
+		} catch (AccessDeniedException e) {
+			Log.warning("Could not access either file " + f1.getAbsolutePath() + " or " + f2.getAbsolutePath());
+			return true;		// TODO: Cleaner solution
+		}
+
 		byte[] f1hash = MessageDigest.getInstance("MD5").digest(f1bytes);
 		byte[] f2hash = MessageDigest.getInstance("MD5").digest(f2bytes);
 		
---
+++ src/iis/tommasosalvini/pcrestorer/utils/FolderUtils.java
@@ -0,0 +1,47 @@
+package iis.tommasosalvini.pcrestorer.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class FolderUtils {
+
+	public static ArrayList<File> flatten(File f1) throws IOException {
+		ArrayList<File> flattened = new ArrayList<File>();
+		Path[] contents;
+		try {
+			contents = (Path[]) Files.list(f1.toPath()).toArray();
+		} catch (AccessDeniedException e) {
+			return new ArrayList<File>();
+		}
+		for (Path content : contents) {
+			if (content.toFile().isDirectory()) {
+				ArrayList<File> subContents = FolderUtils.flatten(content.toFile());
+				flattened.addAll(subContents);
+				flattened.add(content.toFile());
+			} else {
+				if (!(content.toFile().getName().equals("desktop.ini"))) flattened.add(content.toFile());
+			}
+		}
+		return flattened;
+	}
+	
+	public static boolean compare(File f1, File f2) throws IOException, NoSuchAlgorithmException {
+		ArrayList<File> c1 = FolderUtils.flatten(f1);
+		ArrayList<File> c2 = FolderUtils.flatten(f2);
+		ArrayList<String> c1names = new ArrayList<String>();
+		ArrayList<String> c2names = new ArrayList<String>();
+		for (File fc1 : c1) c1names.add(fc1.getName());
+		for (File fc2 : c2) c2names.add(fc2.getName());
+
+		if (c1.size() != c2.size()) return false;
+		for (int i = 0; i < c1names.size(); i++) if (!(c1names.get(i).equals(c2names.get(i)))) return false;
+		for (int i = 0; i < c1names.size(); i++) if (!(FileUtils.compare(c1.get(i), c2.get(i)))) return false;
+		return true;
+	}
+
+}
